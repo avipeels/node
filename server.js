3b79036c835ac86102666ad44345dc51a901012a
Peelukhana Sai Krishna Avinash <avipeelu@publicisgroupe.net>
2020-04-15 12:45:51 +0530

    added express middleware

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
 
-server.listen(3000);
+app.use('/',(req, res, next) => {
+    console.log('this always runs');
+    next();
+});
+
+
+app.use('/add-product',(req, res, next) => {
+    console.log('in add product');
+    res.send('<h1>add product page</h1>');
+});
+
+app.use('/',(req, res, next) => {
+    console.log('in another middle ware');
+    res.send('<h1>hello from express js</h1>');
+    console.log('---');
+});
+
+app.listen(3000);
+console.log('server listening on 3000');
