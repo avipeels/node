a29b35e512985b590a5a6cd51ffecb25d7b880d0
Peelukhana Sai Krishna Avinash <avipeelu@publicisgroupe.net>
2020-04-15 13:27:37 +0530

    Router for express

--- server.js
+++ server.js
@@ -3,19 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>hello from express js</h1>');
-});
 
 app.listen(3000);
