4e8d9a7a9e43740c1197111c2f885dd4b0dff900
Peelukhana Sai Krishna Avinash <avipeelu@publicisgroupe.net>
2020-04-15 19:40:37 +0530

    404 error,path,css,

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,13 @@
 const express = require('express');
-
+const path = require('path');
 const router = express.Router();
+const rootDir = require('../utils/path');
 
 router.get('/add-product', (req, res, next) => {
-    res.send(
-        '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 
-router.post('/product', (req, res) => {
+router.post('/add-product', (req, res) => {
     console.log(req.body);
     res.redirect('/');
 });
