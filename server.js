52814ad410a3284f987cb48de7857145434c4c05
Peelukhana Sai Krishna Avinash <avipeelu@publicisgroupe.net>
2020-04-15 13:10:49 +0530

    imcoming data parsing: express get post bodyParser

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 const http = require('http');
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/',(req, res, next) => {
-    console.log('this always runs');
-    next();
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
+app.post('/product', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+})
 
-app.use('/add-product',(req, res, next) => {
-    console.log('in add product');
-    res.send('<h1>add product page</h1>');
-});
-
-app.use('/',(req, res, next) => {
-    console.log('in another middle ware');
+app.use('/', (req, res, next) => {
     res.send('<h1>hello from express js</h1>');
-    console.log('---');
 });
 
 app.listen(3000);
-console.log('server listening on 3000');
