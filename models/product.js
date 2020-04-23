8137cb396459c6e0fe9c939094d538095d363dc9
Peelukhana Sai Krishna Avinash <avipeelu@publicisgroupe.net>
2020-04-23 14:24:57 +0530

    reusing code in models

--- models/product.js
+++ models/product.js
@@ -1,37 +1,36 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json');
+
+const getProductFromFile = (cb) => {
+    fs.readFile(p, (error, fileContent) => {
+        if (error) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    })
+}
+
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
 
     save() {
-        const p = path.join(path.dirname(process.mainModule.filename),
-            'data',
-            'products.json');
-        fs.readFile(p, (error, fileContent) => {
-            let products = [];
-            if (!error) {
-                products = JSON.parse(fileContent);
-            }
+        getProductFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (error) => {
                 console.log(error);
             });
-        })
+        });
     }
 
     static fetctAll(cb) {
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json');
-        fs.readFile(p, (error, fileContent) => {
-            if (error) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        })
+        getProductFromFile(cb);
     }
 }
