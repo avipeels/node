ce11fb0159677c5cfb4c116bb83a223effb89f2f
Peelukhana Sai Krishna Avinash <avipeelu@publicisgroupe.net>
2020-04-23 14:15:19 +0530

    added model, save n retrieve data from file

---
+++ models/product.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title;
+    }
+
+    save() {
+        const p = path.join(path.dirname(process.mainModule.filename),
+            'data',
+            'products.json');
+        fs.readFile(p, (error, fileContent) => {
+            let products = [];
+            if (!error) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (error) => {
+                console.log(error);
+            });
+        })
+    }
+
+    static fetctAll(cb) {
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            'data',
+            'products.json');
+        fs.readFile(p, (error, fileContent) => {
+            if (error) {
+                cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        })
+    }
+}
